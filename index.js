e1f918c50f51aa32a7d1507598a1a688cbd87e9f
Rohit Saini <rohitsaini0444@gmail.com>
2022-02-17 01:08:53 +0530

    Added database connection

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express')
+const mongoose = require('mongoose');
+const express = require('express');
 const app = express();
 const port = 3000;
+
+//Set up default mongoose connection
+const mongoDB = 'mongodb://127.0.0.1/newsapp';
+const connect = mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+connect.then(
+    db => {
+      console.log("Connected to the mongodb server correctly ");
+    },
+    err => {
+      console.log("MongoDB connection error:",err);
+    }
+  );
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
